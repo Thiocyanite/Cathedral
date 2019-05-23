ce88448b21c29628c092b4137f0d31b05543fc71
Waneksio <waneksiomiszczu@gmail.com>
2019-05-23 18:13:52 +0200

    added observer class and modified existing methods

---
+++ Source/Observer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by matt on 23.05.19.
+//
+
+#include "Observer.h"
---
+++ Source/Observer.h
@@ -0,0 +1,14 @@
+//
+// Created by matt on 23.05.19.
+//
+
+#ifndef OPENGLSETUP_OBSERVER_H
+#define OPENGLSETUP_OBSERVER_H
+
+
+class Observer {
+
+};
+
+
+#endif //OPENGLSETUP_OBSERVER_H
